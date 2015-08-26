0eefc98d93d252a63774d9e86bd0d6d3a9a70d7b
Nabil Gardon <nabil.gardon@gmail.com>
2015-08-26 17:25:26 +0200

    Reservation avant paiement

---
+++ Form114/Controllers/ReservationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Form114.Models;
+
+namespace Form114.Controllers
+{
+    public class ReservationController : Controller
+    {
+        // GET: Reservation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Reserver(ReservationViewModel rvm)
+        {
+            return View(rvm);
+        }
+
+        public ActionResult Reserver()
+        {
+            return RedirectToAction("Details", "Produit", new { id = 1 });
+        }
+    }
+}
---
+++ Form114/Models/ReservationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Form114.Models
+{
+    public class ReservationViewModel
+    {
+        public int IdProduit { get; set; }
+        public DateTime DateDebut { get; set; }
+        public DateTime DateFin { get; set; }
+        public int Prix { get; set; }
+    }
+}
