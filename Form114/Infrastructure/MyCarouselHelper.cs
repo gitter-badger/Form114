0ddf6d1506bfdd45ded32c3cab547680f2fbca21
MMarieJoseph <matthieumariejoseph@outlook.com>
2015-08-27 15:43:11 +0200

    Carousel V2

--- Form114/Infrastructure/MyCarouselHelper.cs
+++ Form114/Infrastructure/MyCarouselHelper.cs
@@ -8,89 +8,120 @@ namespace Form114.Infrastructure
 {
     public static class MyCarouselHelper
     {
-        public static MvcHtmlString MyCarousel(this HtmlHelper self, List<string> listePhotos, string id)
+        public static MvcHtmlString MyCarousel(this HtmlHelper self, List<string> listePhotos)
         {
-
-            var divTag = new TagBuilder("div");
-            divTag.AddCssClass("carousel slide carousel-height");
-            divTag.Attributes.Add("id", id);
-            divTag.Attributes.Add("data-ride", "carousel");
-
-            var olTag = new TagBuilder("ol");
-            olTag.AddCssClass("carousel-indicators");
-
-            var divInnerTag = new TagBuilder("div");
-            divInnerTag.AddCssClass("carousel-inner");
-            divInnerTag.Attributes.Add("role", "listbox");
-            
-            for (int i = 0; i < listePhotos.Count; i++)
+            var str = "";
+            str += "<div id=\"myCarousel\" class=\"carousel slide\" data-ride=\"carousel\">";
+            str += "<div class=\"carousel-inner\" role=\"listbox\">";
+            for (int i = 0; i < listePhotos.Count(); i++ )
             {
-                var divImageTag = new TagBuilder("div");
-                var imgTag = new TagBuilder("img");
-                var liTag = new TagBuilder("li");
-                liTag.Attributes.Add("data-target", "#" + id);
                 if (i == 0)
                 {
-                    liTag.AddCssClass("active");
-                    divImageTag.AddCssClass("item active");
+                    str += "<div class=\"item active\">";
+                    str += "<div class=\"cadre\">";
+                    str += "<img src=\"/Images/"+listePhotos[i].Trim()+"\" alt=\"Pas de photo\" class=\"MonImage\"/>";
+                    str += "</div>";
+                    str += "</div>";
                 }
                 else
-                    divImageTag.AddCssClass("item");
-
-                liTag.Attributes.Add("data-slide-to", i.ToString());
-                olTag.InnerHtml += liTag.ToString();
-
-                imgTag.Attributes.Add("src", "/Images/"+listePhotos[i]);
-                imgTag.Attributes.Add("alt", "Image indisponible");
-                imgTag.AddCssClass("image-carousel");
-                divImageTag.InnerHtml = imgTag.ToString();
-                divInnerTag.InnerHtml += divImageTag.ToString();
+                {
+                    str += "<div class=\"item\">";
+                    str += "<div class=\"cadre\">";
+                    str += "<img src=\"/Images/" + listePhotos[i].Trim() + "\" alt=\"Pas photo\" class=\"MonImage\"/>";
+                    str += "</div>";
+                    str += "</div>";
+                }
             }
-            divTag.InnerHtml += olTag.ToString();
-            divTag.InnerHtml += divInnerTag.ToString();
-            string s = "  <a class='left carousel-control' href=\"#" + id + "\" role='button' data-slide='prev'>"+
-                    @"<span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>
-                    <span class='sr-only'>Previous</span>
-                    </a>"+
-                    "<a class='right carousel-control' href=\"#" + id + "\" role='button' data-slide='next'>"+
-                    @"<span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span>
-                    <span class='sr-only'>Next</span>
-                    </a>";
+            str += "</div>";
+            str += "<a class=\"left carousel-control\" href=\"#myCarousel\" role=\"button\" data-slide=\"prev\">";
+            str += "<span class=\"glyphicon glyphicon-chevron-left\" aria-hidden=\"true\"></span>";
+            str += "<span class=\"sr-only\">Previous</span>";
+            str += "</a>";
+            str += "<a class=\"right carousel-control\" href=\"#myCarousel\" role=\"button\" data-slide=\"next\">";
+            str += "<span class=\"glyphicon glyphicon-chevron-right\" aria-hidden=\"true\"></span>";
+            str += "<span class=\"sr-only\">Next</span>";
+            str += "</a></div>";
 
-            divTag.InnerHtml += s;
 
-            return new MvcHtmlString(divTag.ToString());
-            //            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            //  <!-- Indicators -->
-            //  <ol class="carousel-indicators">
-            //    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            //    <li data-target="#myCarousel" data-slide-to="1"></li>
-            //    <li data-target="#myCarousel" data-slide-to="2"></li>
-            //    <li data-target="#myCarousel" data-slide-to="3"></li>
-            //  </ol>
+//            div id="myCarousel" class="carousel slide" data-ride="carousel">
+//    <div class="carousel-inner" role="listbox">
+//        <div class="item active">
+//            <div class="cadre">
+//            <img src="~/Images/@Model.ListPhotos[0]" alt="Pas de photo" class="MonImage"/>
+//            </div>
+//        </div>
+//        <div class="item">
+//            <div class="cadre">
+//            <img src="~/Images/@Model.ListPhotos[1]" alt="Pas de photo" class="MonImage" />
+//            </div>
+//        </div>
+//    </div>
+//    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+//        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+//        <span class="sr-only">Previous</span>
+//    </a>
+//    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+//        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+//        <span class="sr-only">Next</span>
+//    </a>
+//</div>
 
-            //  <!-- Wrapper for slides -->
-            //  <div class="carousel-inner" role="listbox">
-            //    <div class="item active">
-            //      <img src="img_chania.jpg" alt="Chania">
-            //    </div>
 
-            //    <div class="item">
-            //      <img src="img_chania2.jpg" alt="Chania">
-            //    </div>
 
-            //    <div class="item">
-            //      <img src="img_flower.jpg" alt="Flower">
-            //    </div>
 
-            //    <div class="item">
-            //      <img src="img_flower2.jpg" alt="Flower">
-            //    </div>
-            //  </div>
 
-            //  <!-- Left and right controls -->
 
-            //</div>
+
+//            var divTag = new TagBuilder("div");
+//            divTag.AddCssClass("carousel slide carousel-height");
+//            divTag.Attributes.Add("id", id);
+//            divTag.Attributes.Add("data-ride", "carousel");
+
+//            var olTag = new TagBuilder("ol");
+//            olTag.AddCssClass("carousel-indicators");
+
+//            var divInnerTag = new TagBuilder("div");
+//            divInnerTag.AddCssClass("carousel-inner");
+//            divInnerTag.Attributes.Add("role", "listbox");
+            
+//            for (int i = 0; i < listePhotos.Count; i++)
+//            {
+//                var divImageTag = new TagBuilder("div");
+//                var imgTag = new TagBuilder("img");
+//                var liTag = new TagBuilder("li");
+//                liTag.Attributes.Add("data-target", "#" + id);
+//                if (i == 0)
+//                {
+//                    liTag.AddCssClass("active");
+//                    divImageTag.AddCssClass("item active");
+//                }
+//                else
+//                    divImageTag.AddCssClass("item");
+
+//                liTag.Attributes.Add("data-slide-to", i.ToString());
+//                olTag.InnerHtml += liTag.ToString();
+
+//                imgTag.Attributes.Add("src", "/Images/"+listePhotos[i]);
+//                imgTag.Attributes.Add("alt", "Image indisponible");
+//                imgTag.AddCssClass("image-carousel");
+//                divImageTag.InnerHtml = imgTag.ToString();
+//                divInnerTag.InnerHtml += divImageTag.ToString();
+//            }
+//            divTag.InnerHtml += olTag.ToString();
+//            divTag.InnerHtml += divInnerTag.ToString();
+//            string s = "  <a class='left carousel-control' href=\"#" + id + "\" role='button' data-slide='prev'>"+
+//                    @"<span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>
+//                    <span class='sr-only'>Previous</span>
+//                    </a>"+
+//                    "<a class='right carousel-control' href=\"#" + id + "\" role='button' data-slide='next'>"+
+//                    @"<span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span>
+//                    <span class='sr-only'>Next</span>
+//                    </a>";
+
+//            divTag.InnerHtml += s;
+
+            return new MvcHtmlString(str);
+
         }
     }
 }
